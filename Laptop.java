12bf2def9cd5e529577e4b4b70c42e0c89f96343
Kadir Ödek <oed260434@spengergasse.at>
2026-09-23 20:59:42 +0200

    Class hinzugefügt

--- Laptop.java
+++ Laptop.java
@@ -3,10 +3,5 @@ public class Laptop
     private String hersteller;
     private int kosten;
     private boolean neu;
-    
-    public Laptop (String hersteller, int kosten, boolean neu)
-    {
-        
-    }
 }
 
