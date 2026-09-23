e1b6fd52a95b17f86b853d2ee978b657454c1678
Kadir Ödek <oed260434@spengergasse.at>
2026-09-23 20:57:53 +0200

    Initial sharing of project

---
+++ Laptop.java
@@ -0,0 +1,12 @@
+public class Laptop
+{
+    private String hersteller;
+    private int kosten;
+    private boolean neu;
+    
+    public Laptop (String hersteller, int kosten, boolean neu)
+    {
+        
+    }
+}
+
