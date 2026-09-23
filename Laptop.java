e893ccd235aaf08408627cb1b5828efae7423eaa
Kadir Ödek <oed260434@spengergasse.at>
2026-09-23 21:25:00 +0200

    Konstruktoren ergänzt

--- Laptop.java
+++ Laptop.java
@@ -10,8 +10,24 @@ public class Laptop
         setKosten(neuKosten);
         setNeu(neuNeu);
     }
+    
+    public Laptop(String neuHersteller, boolean neuNeu)
+    {
+        setHersteller(neuHersteller);
+        setKosten(1000);
+        setNeu(neuNeu);
+    }
+    
+    public Laptop()
+    {
+        setHersteller("Lenovo");
+        setKosten(1000);
+        setNeu(true);
+    }
    
     
+    
+
     
     
     
