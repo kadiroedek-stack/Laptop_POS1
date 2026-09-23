80b04ce1f270ead97e279e7528508c3de6415ad6
Kadir Ödek <oed260434@spengergasse.at>
2026-09-23 21:17:41 +0200

    Konstruktor hinzugefügt

--- Laptop.java
+++ Laptop.java
@@ -3,5 +3,22 @@ public class Laptop
     private String hersteller;
     private int kosten;
     private boolean neu;
+    
+    public Laptop(String neuHersteller, int neuKosten, boolean neuNeu)
+    {
+        setHersteller(neuHersteller);
+        setKosten(neuKosten);
+        setNeu(neuNeu);
+    }
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
